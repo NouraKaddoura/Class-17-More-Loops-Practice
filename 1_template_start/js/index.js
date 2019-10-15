b96a304759e350f94a53c97520363f9ba28d93bc
Noura Kaddoura <nourakaddoura@gmail.com>
2019-10-14 17:27:00 -0700

    Class 17

---
+++ 1_template_start/js/index.js
@@ -0,0 +1,9 @@
+$(function(){ //Document.ready() shorthand
+
+    // Build your arrays
+    // Add products to the store
+    // once we have a product design we like, "templatize" it here
+  
+
+
+});
